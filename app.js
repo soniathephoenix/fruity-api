315644f5a7a410134be8f5111ced8cee67abb2db
Sonia Pires <soniacodesloads@gmail.comgit config --global user.name soniathephoenix>
2025-01-21 09:56:53 +0000

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const logger = require("./logger")
 
 //Middleware
+app.use(cors())
 app.use(express.json())
 app.use(logger)
 
